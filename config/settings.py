59585f443809f9179ea706effe975be6a35f2cef
Victor Pereira <victtor.vpb@gmail.com>
2019-03-01 23:36:54 -0300

    remove comment settings database sqlite3

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,6 @@ DATABASES = {
         'PASSWORD': '123456',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
