8c37bd34f73cf3f4161490269ad41054b1b1b85e
Victor Pereira <victtor.vpb@gmail.com>
2019-03-05 20:31:42 -0300

    create model payload

---
+++ apps/api/models/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Payload(models.Model):
+
+    nome = models.CharField(max_length=200)
+    sobrenome = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    defaul_language = models.CharField(max_length=200)
+    data_nascimento = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200)
+    area_interesse = models.CharField(max_length=200)
+    escola = models.CharField(max_length=200)
+    status_escola = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'Nome {self.name} - Sobrenome{self.sobrenome}'
+    
