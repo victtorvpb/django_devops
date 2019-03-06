a6b7dfb987eb66667050b198f9054b438be5de92
Victor Pereira <victtor.vpb@gmail.com>
2019-03-05 21:13:14 -0300

    refactory model

--- apps/api/models/models.py
+++ apps/api/models/models.py
@@ -7,12 +7,12 @@ class Payload(models.Model):
     sobrenome = models.CharField(max_length=200)
     email = models.EmailField()
     password = models.CharField(max_length=200)
-    defaul_language = models.CharField(max_length=200)
-    data_nascimento = models.CharField(max_length=200)
+    defaultLanguage = models.CharField(max_length=200)
+    dataNascimento = models.CharField(max_length=200)
     endereco = models.CharField(max_length=200)
-    area_interesse = models.CharField(max_length=200)
+    areasInteresse = models.CharField(max_length=200)
     escola = models.CharField(max_length=200)
-    status_escola = models.CharField(max_length=200)
+    statusEscola = models.CharField(max_length=200)
 
     def __str__(self):
         return f"Nome {self.name} - Sobrenome{self.sobrenome}"
